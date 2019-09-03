bf829b73b7dec5718f0a5daa3a5a3367436f8d33
rowpure <rowpure@gmail.com>
2019-09-03 17:24:31 +0900

    66

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,9 +2,14 @@
 namespace App\Controller;
 
 class HelloController extends AppController {
+  public $name = 'Hello';
+  public $autoRender = false;
+
   public function index(){
-    $this->name = 'Hello';
-    $this->autoRender = false;
     echo "hello world!";
+  }
+
+  public function other(){
+    echo "これは、index以外の表示です。";
   }
 }
