4a05f3b38516ff5859b56982329e171887a1d24e
rowpure <rowpure@gmail.com>
2019-09-05 15:59:44 +0900

    94

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -19,6 +19,6 @@ class HelloController extends AppController {
     } else {
       $result = "empty.";
     }
-    $this->set("result", htmlspecialchars($result));
+    $this->set("result", h($result));
   }
 }
