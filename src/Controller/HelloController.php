9db1db9c38c9841c6dd6b938f140c02a5395c535
rowpure <rowpure@gmail.com>
2019-09-05 12:00:15 +0900

    77

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,17 +4,10 @@ namespace App\Controller;
 class HelloController extends AppController {
   public function initialize(){
     $this->name = 'Hello';
-    $this->autoRender = false;
-    $this->viewBuilder()->autoLayout(false);
+    $this->viewBuilder()->autoLayout(true);
+    $this->viewBuilder()->layout('Hello');
   }
 
   public function index(){
-    $this->viewBuilder()->autoLayout(true);
-    $this->autoRender = true;
-  }
-
-  public function other(){
-    $this->viewBuilder()->autoLayout(false);
-    $this->autoRender = true;
   }
 }
