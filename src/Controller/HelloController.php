52622cf4103b0e0accf02083872b0b05a24ab9a2
rowpure <rowpure@gmail.com>
2019-09-05 11:21:29 +0900

    75

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,9 +2,19 @@
 namespace App\Controller;
 
 class HelloController extends AppController {
-  public $name = 'Hello';
-  public $autoRender = true;
+  public function initialize(){
+    $this->name = 'Hello';
+    $this->autoRender = false;
+    $this->viewBuilder()->autoLayout(false);
+  }
 
   public function index(){
+    $this->viewBuilder()->autoLayout(true);
+    $this->autoRender = true;
+  }
+
+  public function other(){
+    $this->viewBuilder()->autoLayout(false);
+    $this->autoRender = true;
   }
 }
---
+++ src/Template/Hello/other.ctp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta http-equiv="content-type" content="text/html;
+      charset=UTF-8">
+    <title>Hello Page</title>
+  </head>
+
+  <body>
+    <h1>サンプル見出し</h1>
+    <p>これは、index以外の表示です。</p>
+  </body>
+</html>
