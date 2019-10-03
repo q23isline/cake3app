3beb936c76930fc320b8125ce5e12a66ea3fd59b
rowpure <rowpure@gmail.com>
2019-10-03 21:18:30 +0900

    今後Sqliteを使用するよう変更

--- config/app.php
+++ config/app.php
@@ -254,19 +254,13 @@ return [
     'Datasources' => [
         'default' => [
             'className' => 'Cake\Database\Connection',
-            'driver' => 'Cake\Database\Driver\Mysql',
+            'driver' => 'Cake\Database\Driver\Sqlite',
             'persistent' => false,
-            'host' => 'localhost',
-            'username' => 'root',
+            'username' => '',
             'password' => '',
-            'database' => 'mydata',
+            'database' => ROOT . DS . 'db' . DS . 'mydata.sqlite3',
             'encoding' => 'utf8',
-            'timezone' => 'UTC',
-            'flags' => [],
             'cacheMetadata' => true,
-            'log' => false,
-            'quoteIdentifiers' => false,
-            'url' => env('DATABASE_URL', null),
         ],
     ],
 
