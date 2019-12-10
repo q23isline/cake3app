e709eac794548d88cd43e8ed4746df676c91419c
rowpure <rowpure@gmail.com>
2019-12-10 23:47:03 +0900

    176

--- src/Controller/BoardsController.php
+++ src/Controller/BoardsController.php
@@ -6,14 +6,20 @@ use \Exception;
 use Cake\Log\Log;
 
 class BoardsController extends AppController {
-  public function index(){
+  public function index($id = null){
     $this->set('entity', $this->Boards->newEntity());
-    if ($this->request->is('post')) {
-      $id = $this->request->data['id'];
-      $data = $this->Boards->findByIdOrName($id, $id);
-    } else {
-      $data = $this->Boards->find('all');
+    if ($id != null) {
+      try {
+        $entity = $this->Boards->get($id);
+        $this->set('entity', $entity);
+      } catch(Exception $e) {
+        // Logg:write('debug', $e->getMessage());
+        // ↑スペル誤り　↓が正しい
+        Log::write('debug', $e->getMessage());
+      }
     }
+
+    $data = $this->Boards->find('all')->order(['id'=>'DESC']);
     $this->set('data', $data->toArray());
     $this->set('count', $data->count());
   }
@@ -22,6 +28,22 @@ class BoardsController extends AppController {
     if ($this->request->is('post')){
       $board = $this->Boards->newEntity($this->request->data);
       $this->Boards->save($board);
+    }
+
+    return $this->redirect(['action' => 'index']);
+  }
+
+  public function editRecord() {
+    if ($this->request->is('put')) {
+      try {
+        $entity = $this->Boards->get($this->request->data['id']);
+        $this->Boards->patchEntity($entity, $this->request->data);
+        $this->Boards->save($entity);
+      } catch(Exception $e) {
+        // Logg:write('debug', $e->getMessage());
+        // ↑スペル誤り　↓が正しい
+        Log::write('debug', $e->getMessage());
+      }
     }
 
     return $this->redirect(['action' => 'index']);
