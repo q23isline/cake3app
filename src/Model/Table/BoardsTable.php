ce76daac7e41e2e5b7acdea515c8641f38ba10da
rowpure <rowpure@gmail.com>
2019-12-16 19:37:51 +0900

    203

--- src/Model/Table/BoardsTable.php
+++ src/Model/Table/BoardsTable.php
@@ -4,4 +4,9 @@ namespace App\Model\Table;
 use Cake\ORM\Table;
 
 class BoardsTable extends Table {
+  public function initialize(array $config) {
+    parent::initialize($config);
+
+    $this->table('books');
+  }
 }
