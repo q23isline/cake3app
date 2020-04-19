ee09d2680faf98cbd4c843bc5b7a7b048395f0d0
rowpure <rowpure@gmail.com>
2020-04-19 22:49:56 +0900

    492 インタラクティブなシェル

--- src/Shell/MyCmdShell.php
+++ src/Shell/MyCmdShell.php
@@ -4,7 +4,6 @@ namespace App\Shell;
 use Cake\Console\ConsoleOptionParser;
 use Cake\Console\Shell;
 use Cake\Log\Log;
-use Cake\ORM\TableRegistry;
 
 class MyCmdShell extends Shell
 {
@@ -16,18 +15,40 @@ class MyCmdShell extends Shell
     public function initialize()
     {
         parent::initialize();
-        $this->loadModel('Boards');
     }
 
     /**
      * 主処理
      *
-     * @param int $num BoardsテーブルのID
      * @return void
      */
-    public function main($num)
+    public function main()
     {
-        $data = $this->Boards->get($num);
+        $this->out('※以下のテーブルが利用できます。');
+        $this->out('[B]oards');
+        $this->out('[P]eople');
+        $t = $this->in('テーブルを選択: ', ['B', 'P'], 'B');
+        $t = strtoupper($t);
+        $table = null;
+        $id = $this->in('ID番号を入力: ', null, 1);
+        $data = null;
+        switch ($t) {
+            case 'B':
+                $table = 'Boards';
+                $this->loadModel('Boards');
+                $data = $this->Boards->get($id);
+                break;
+            case 'P':
+                $table = 'People';
+                $this->loadModel('People');
+                $data = $this->People->get($id);
+                break;
+            default:
+                $this->info("can't access Database...");
+                exit();
+        }
+        $this->out();
+        $this->out("※{$table} id={$id} のレコード: ");
         $this->out(print_r($data->toArray()));
     }
 }
