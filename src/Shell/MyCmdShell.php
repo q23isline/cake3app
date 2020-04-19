478b12578ccb017184331fd0232bd0fec21d9a87
rowpure <rowpure@gmail.com>
2020-04-19 22:23:26 +0900

    490

--- src/Shell/MyCmdShell.php
+++ src/Shell/MyCmdShell.php
@@ -4,21 +4,30 @@ namespace App\Shell;
 use Cake\Console\ConsoleOptionParser;
 use Cake\Console\Shell;
 use Cake\Log\Log;
+use Cake\ORM\TableRegistry;
 
 class MyCmdShell extends Shell
 {
     /**
-     * 主処理
+     * 初期化
      *
-     * @param array $num 足したい数値
      * @return void
      */
-    public function main(...$num)
+    public function initialize()
     {
-        $res = 0;
-        foreach ($num as $n) {
-            $res += $n;
-        }
-        $this->out('合計: ' . $res);
+        parent::initialize();
+        $this->loadModel('Boards');
+    }
+
+    /**
+     * 主処理
+     *
+     * @param int $num BoardsテーブルのID
+     * @return void
+     */
+    public function main($num)
+    {
+        $data = $this->Boards->get($num);
+        $this->out(print_r($data->toArray()));
     }
 }
